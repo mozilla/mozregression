2232835f936204274b96e1a51c1b6ffb8e4faa78
harthur <fayearthur@gmail.com>
2010-09-15 19:23:46 -0700

    add license headers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [mozregression](http://harthur.github.com/mozregression) is an interactive regression range finder for Mozilla nightly builds.
 
 #install
-you can install mozregression with [setuptools](http://pypi.python.org/pypi/setuptools) or [pip](http://pypi.python.org/pypi/pip)
+you can install mozregression with [setuptools](http://pypi.python.org/pypi/setuptools) or [pip](http://pypi.python.org/pypi/pip). [OS-specific instructions](http://harthur.github.com/mozregression/)
 
 	easy_install mozregression
 
@@ -10,7 +10,7 @@ you can install mozregression with [setuptools](http://pypi.python.org/pypi/setu
 To find the regression range for a bug, supply mozregression with the date of the last known good nightly and the date of a bad nightly (optional):
 	mozregression --good=2010-03-16 --bad=2010-09-08
 	
-This will download and run several nightly builds and narrow down the range. After checking a few nightlies, you'll get the regression range which looks something like this:
+This will download and run several nightly builds on new profiles and narrow down the range. After checking a few nightlies, you'll get the regression range which looks something like this:
 	Last good nightly: 2010-09-08 First bad nightly: 2010-09-09
 
 	Pushlog: http://hg.mozilla.org/mozilla-central/pushloghtml?fromchange=36f5cf6b2d42&tochange=8e0fce7d5b49
--- mozregression/regression.py
+++ mozregression/regression.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Corporation Code.
+#
+# The Initial Developer of the Original Code is
+# Heather Arthur
+# Portions created by the Initial Developer are Copyright (C) 2009
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s): Heather Arthur <fayearthur@gmail.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 import datetime
 import sys
 from optparse import OptionParser
@@ -10,6 +46,7 @@ class Bisector():
     def __init__(self, runner): 
         self.runner = runner
         self.currAppInfo = ''
+        self.prevAppInfo = ''
         self.currDate = ''
 
     def bisect(self, goodDate, badDate):
@@ -80,7 +117,7 @@ def cli():
     cmdargs = strsplit(options.cmdargs, ",")
     
     if not options.good_date:
-        options.good_date = "2008-01-01"
+        options.good_date = "2009-01-01"
         print "No 'good' date specified, using " + options.good_date
 
     runner = NightlyRunner(appname=options.app, addons=addons, profile=options.profile, cmdargs=cmdargs)

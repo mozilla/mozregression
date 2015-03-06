8213d5e4543698279ffe264e5d5a173e2d13bba3
Julien Pagès <j.parkouss@gmail.com>
2015-03-06 11:22:38 +0100

    split the bisection process so we can use it from the gui

--- mozregression/bisector.py
+++ mozregression/bisector.py
@@ -238,15 +238,137 @@ class InboundHandler(BisectorHandler):
                           % (words[1], self.bad_revision))
 
 
+class Bisection(object):
+    RUNNING = 0
+    NO_DATA = 1
+    FINISHED = 2
+    USER_EXIT = 3
+
+    def __init__(self, handler, build_data, download_manager, test_runner,
+                 fetch_config, dl_in_background=True):
+        self.handler = handler
+        self.build_data = build_data
+        self.download_manager = download_manager
+        self.test_runner = test_runner
+        self.fetch_config = fetch_config
+        self.dl_in_background = dl_in_background
+        self.previous_data = []
+
+    def search_mid_point(self):
+        self.handler.set_build_data(self.build_data)
+        return self._search_mid_point()
+
+    def _search_mid_point(self):
+        return self.build_data.mid_point()
+
+    def init_handler(self, mid_point):
+        if len(self.build_data) == 0:
+            self.handler.no_data()
+            return self.NO_DATA
+
+        self.handler.initialize()
+
+        if mid_point == 0:
+            self.handler.finished()
+            return self.FINISHED
+        return self.RUNNING
+
+    def download_build(self, mid_point):
+        build_infos = self.handler.build_infos(mid_point, self.fetch_config)
+        return self._download_build(mid_point, build_infos)
+
+    def _download_build(self, mid_point, build_infos):
+        dest = self.download_manager.focus_download(build_infos)
+        build_infos['build_path'] = dest
+        if self.dl_in_background:
+            self._download_next_builds(mid_point)
+        return build_infos
+
+    def _download_next_builds(self, mid_point):
+        # start downloading the next builds.
+        # note that we don't have to worry if builds are already
+        # downloaded, or if our build infos are the same because
+        # this will be handled by the downloadmanager.
+        def start_dl(r):
+            # first get the next mid point
+            # this will trigger some blocking downloads
+            # (we need to find the build info)
+            m = r.mid_point()
+            if len(r) != 0:
+                # this is a trick to call build_infos
+                # with the the appropriate build_data
+                self.handler.set_build_data(r)
+                try:
+                    # non-blocking download of the build
+                    self.download_manager.download_in_background(
+                        self.handler.build_infos(m, self.fetch_config))
+                finally:
+                    # put the real build_data back
+                    self.handler.set_build_data(self.build_data)
+        # download next left mid point
+        start_dl(self.build_data[mid_point:])
+        # download right next mid point
+        start_dl(self.build_data[:mid_point+1])
+
+    def evaluate(self, build_infos):
+        return self.test_runner.evaluate(build_infos,
+                                         allow_back=bool(self.previous_data))
+
+    def update_build_info(self, mid_point, app_info):
+        # update build_info in build_data if possible
+        # this is required as some old builds do not have information
+        # on the server
+        build_info = self.build_data[mid_point]
+        build_info.setdefault('changeset',
+                              app_info.get('application_changeset'))
+        build_info.setdefault('repository',
+                              app_info.get('application_repository'))
+
+    def handle_verdict(self, mid_point, verdict):
+        if verdict == 'g':
+            # if build is good and we are looking for a regression, we
+            # have to split from
+            # [G, ?, ?, G, ?, B]
+            # to
+            #          [G, ?, B]
+            self.previous_data.append(self.build_data)
+            if not self.handler.find_fix:
+                self.build_data = self.build_data[mid_point:]
+            else:
+                self.build_data = self.build_data[:mid_point+1]
+            self.handler.build_good(mid_point, self.build_data)
+        elif verdict == 'b':
+            # if build is bad and we are looking for a regression, we
+            # have to split from
+            # [G, ?, ?, B, ?, B]
+            # to
+            # [G, ?, ?, B]
+            self.previous_data.append(self.build_data)
+            if not self.handler.find_fix:
+                self.build_data = self.build_data[:mid_point+1]
+            else:
+                self.build_data = self.build_data[mid_point:]
+            self.handler.build_bad(mid_point, self.build_data)
+        elif verdict == 'r':
+            self.handler.build_retry(mid_point)
+        elif verdict == 's':
+            self.handler.build_skip(mid_point)
+            self.previous_data.append(self.build_data)
+            self.build_data = self.build_data.deleted(mid_point)
+        elif verdict == 'back':
+            self.build_data = self.previous_data.pop(-1)
+        else:
+            # user exit
+            self.handler.user_exit(mid_point)
+            return self.USER_EXIT
+        return self.RUNNING
+
+
 class Bisector(object):
     """
     Handle the logic of the bisection process, and report events to a given
     :class:`BisectorHandler`.
     """
-    NO_DATA = 1
-    FINISHED = 2
-    USER_EXIT = 3
-
     def __init__(self, fetch_config, test_runner, persist=None,
                  dl_in_background=True):
         self.fetch_config = fetch_config
@@ -271,114 +393,37 @@ class Bisector(object):
                                               good,
                                               bad,
                                               **kwargs)
-        download_manager = \
-            BuildDownloadManager(handler._logger,
-                                 self.download_dir)
+
+        return self._bisect(handler, build_data)
+
+    def _bisect(self, handler, build_data):
+        """
+        Starts a bisection for a :class:`mozregression.build_data.BuildData`.
+        """
+        download_manager = BuildDownloadManager(handler._logger,
+                                                self.download_dir)
+
+        bisection = Bisection(handler, build_data, download_manager,
+                              self.test_runner, self.fetch_config,
+                              dl_in_background=self.dl_in_background)
+
         try:
-            return self._bisect(download_manager, handler, build_data)
+            while True:
+                mid = bisection.search_mid_point()
+                result = bisection.init_handler(mid)
+                if result != bisection.RUNNING:
+                    return result
+
+                build_infos = bisection.download_build(mid)
+                verdict, app_info = bisection.evaluate(build_infos)
+                bisection.update_build_info(mid, app_info)
+                result = bisection.handle_verdict(mid, verdict)
+                if result != bisection.RUNNING:
+                    return result
         finally:
             # ensure we cancel any possible download done in the background
             # else python will block until threads termination.
             download_manager.cancel()
-
-    def _bisect(self, download_manager, handler, build_data):
-        """
-        Starts a bisection for a :class:`mozregression.build_data.BuildData`.
-        """
-        previous_data = []
-        while True:
-            handler.set_build_data(build_data)
-            mid = build_data.mid_point()
-
-            if len(build_data) == 0:
-                handler.no_data()
-                return self.NO_DATA
-
-            handler.initialize()
-
-            if mid == 0:
-                handler.finished()
-                return self.FINISHED
-
-            build_infos = handler.build_infos(mid, self.fetch_config)
-            dest = download_manager.focus_download(build_infos)
-
-            # start downloading the next builds.
-            # note that we don't have to worry if builds are already
-            # downloaded, or if our build infos are the same because
-            # this will be handled by the downloadmanager.
-            if self.dl_in_background:
-                def start_dl(r):
-                    # first get the next mid point
-                    # this will trigger some blocking downloads
-                    # (we need to find the build info)
-                    m = r.mid_point()
-                    if len(r) != 0:
-                        # this is a trick to call build_infos
-                        # with the the appropriate build_data
-                        handler.set_build_data(r)
-                        try:
-                            # non-blocking download of the build
-                            download_manager.download_in_background(
-                                handler.build_infos(m, self.fetch_config))
-                        finally:
-                            # put the real build_data back
-                            handler.set_build_data(build_data)
-                # download next left mid point
-                start_dl(build_data[mid:])
-                # download right next mid point
-                start_dl(build_data[:mid+1])
-
-            build_infos['build_path'] = dest
-            verdict, app_info = \
-                self.test_runner.evaluate(build_infos,
-                                          allow_back=bool(previous_data))
-
-            # update build_info in build_data if possible
-            # this is required as some old builds do not have information
-            # on the server
-            build_info = build_data[mid]
-            build_info.setdefault('changeset',
-                                  app_info.get('application_changeset'))
-            build_info.setdefault('repository',
-                                  app_info.get('application_repository'))
-
-            if verdict == 'g':
-                # if build is good and we are looking for a regression, we
-                # have to split from
-                # [G, ?, ?, G, ?, B]
-                # to
-                #          [G, ?, B]
-                previous_data.append(build_data)
-                if not handler.find_fix:
-                    build_data = build_data[mid:]
-                else:
-                    build_data = build_data[:mid+1]
-                handler.build_good(mid, build_data)
-            elif verdict == 'b':
-                # if build is bad and we are looking for a regression, we
-                # have to split from
-                # [G, ?, ?, B, ?, B]
-                # to
-                # [G, ?, ?, B]
-                previous_data.append(build_data)
-                if not handler.find_fix:
-                    build_data = build_data[:mid+1]
-                else:
-                    build_data = build_data[mid:]
-                handler.build_bad(mid, build_data)
-            elif verdict == 'r':
-                handler.build_retry(mid)
-            elif verdict == 's':
-                handler.build_skip(mid)
-                previous_data.append(build_data)
-                build_data = build_data.deleted(mid)
-            elif verdict == 'back':
-                build_data = previous_data.pop(-1)
-            else:
-                # user exit
-                handler.user_exit(mid)
-                return self.USER_EXIT
 
 
 class BisectRunner(object):
@@ -396,7 +441,7 @@ class BisectRunner(object):
     def bisect_nightlies(self, good_date, bad_date):
         handler = NightlyHandler(find_fix=self.options.find_fix)
         result = self.do_bisect(handler, good_date, bad_date)
-        if result == Bisector.FINISHED:
+        if result == Bisection.FINISHED:
             self._logger.info("Got as far as we can go bisecting nightlies...")
             handler.print_range()
             if self.fetch_config.can_go_inbound():
@@ -446,7 +491,7 @@ class BisectRunner(object):
                     message += (" because the repo `%s` was specified"
                                 % self.options.repo)
                 self._logger.info(message + '.')
-        elif result == Bisector.USER_EXIT:
+        elif result == Bisection.USER_EXIT:
             self.print_resume_info(handler)
         else:
             # NO_DATA
@@ -466,10 +511,10 @@ class BisectRunner(object):
         # expire)
         handler = InboundHandler(find_fix=self.options.find_fix)
         result = self.do_bisect(handler, good_rev, bad_rev, range=60*60*12)
-        if result == Bisector.FINISHED:
+        if result == Bisection.FINISHED:
             self._logger.info("Oh noes, no (more) inbound revisions :(")
             handler.print_range()
-        elif result == Bisector.USER_EXIT:
+        elif result == Bisection.USER_EXIT:
             self.print_resume_info(handler)
         else:
             # NO_DATA

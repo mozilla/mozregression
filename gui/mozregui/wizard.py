38ef4131267a5d0747fb584fadd80e5f58409668
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-05-01 16:39:08 +0530

    Hiding the bits field when the application is 'fennec' or 'fennec-2.3'. The 'fennec' application can only run on a connected android device- so we don't care about the bits here.

--- gui/mozregui/wizard.py
+++ gui/mozregui/wizard.py
@@ -89,6 +89,13 @@ class IntroPage(WizardPage):
         if old_bisect_index == 1 and len(bisect_types) == 2:
             bisect_index = 1
         self.ui.bisect_combo.setCurrentIndex(bisect_index)
+        available_bits = self.fetch_config.available_bits()
+        if not available_bits:
+            self.ui.bits_combo.hide()
+            self.ui.label_4.hide()
+        else:
+            self.ui.bits_combo.show()
+            self.ui.label_4.show()
 
     def validatePage(self):
         app_name = self.fetch_config.app_name

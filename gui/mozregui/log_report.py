78922571e5f6baef46fde53d397c269bfe2d39bc
MikeLing <sabergeass@gmail.com>
2015-07-01 13:02:53 +0800

    use setMaximumBlockCount instead of block counter

--- gui/mozregui/log_report.py
+++ gui/mozregui/log_report.py
@@ -1,24 +1,19 @@
 from PyQt4.QtCore import QObject, pyqtSlot as Slot, pyqtSignal as Signal
-from PyQt4.QtGui import QPlainTextEdit, QTextCursor
+from PyQt4.QtGui import QPlainTextEdit
 from datetime import datetime
 
 
 class LogView(QPlainTextEdit):
+    def __init__(self, parent=None):
+        QPlainTextEdit.__init__(self, parent)
+        self.setMaximumBlockCount(1000)
+
     @Slot(dict)
     def on_log_received(self, data):
         time_info = datetime.fromtimestamp((data['time']/1000)).isoformat()
         log_message = '%s: %s : %s' % (
             time_info, data['level'], data['message'])
         self.appendPlainText(log_message)
-        counter = self.blockCount()
-
-        # do not display more than 1000 log lines
-        if counter > 1000:
-            document = self.document()
-            cursor = QTextCursor(document.findBlockByLineNumber(0))
-            cursor.select(QTextCursor.BlockUnderCursor)
-            cursor.deleteChar()
-            cursor.deleteChar()
 
 
 class LogModel(QObject):

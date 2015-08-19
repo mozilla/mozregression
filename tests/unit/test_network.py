d1ad66d150c6f8fdcbadd73abe871cdef8fb2433
Julien Pagès <j.parkouss@gmail.com>
2015-08-19 10:08:33 +0200

    add a test for network.set_http_session

--- tests/unit/test_network.py
+++ tests/unit/test_network.py
@@ -33,5 +33,26 @@ class TestUrlLinks(unittest.TestCase):
             network.url_links('', regex="thing2.*"),
             ['thing2/'])
 
+
+def test_set_http_session():
+    try:
+        with patch('requests.Session') as Session:
+            session = Session.return_value = Mock()
+            session_get = session.get
+
+            network.set_http_session(get_defaults={'timeout': 5})
+
+        assert session == network.get_http_session()
+        # timeout = 5 will be passed to the original get method as a default
+        session.get('http://my-ul')
+        session_get.assert_called_with('http://my-ul', timeout=5)
+        # if timeout is defined, it will override the default
+        session.get('http://my-ul', timeout=10)
+        session_get.assert_called_with('http://my-ul', timeout=10)
+
+    finally:
+        # remove the global session to not impact other tests
+        network.SESSION = None
+
 if __name__ == '__main__':
     unittest.main()

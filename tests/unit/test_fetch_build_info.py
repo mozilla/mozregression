0a684bddc7239f4487892486369a0952df14a7ba
Julien Pagès <j.parkouss@gmail.com>
2015-10-21 13:14:34 +0200

    a.m.o is broken, use ftp-origin-scl3.mozilla.org
    As a temporary workaround.

--- tests/unit/test_fetch_build_info.py
+++ tests/unit/test_fetch_build_info.py
@@ -63,7 +63,7 @@ class TestNightlyInfoFetcher(unittest.TestCase):
             'bar/'
         ]
         urls = self.info_fetcher._get_urls(datetime.date(2014, 11, 01))
-        self.assertEqual(urls[0], 'https://archive.mozilla.org/pub/mozilla.org/\
+        self.assertEqual(urls[0], 'http://ftp-origin-scl3.mozilla.org/pub/\
 firefox/nightly/2014/11/2014-11-01-03-02-05-mozilla-central/')
         urls = self.info_fetcher._get_urls(datetime.date(2014, 11, 02))
         self.assertEqual(urls, [])

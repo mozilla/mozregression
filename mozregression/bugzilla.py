9e073842f16cbaeede423621e0304ccf2c6ee955
Julien Pagès <j.parkouss@gmail.com>
2016-01-15 10:20:14 +0100

    Bug 1239699 - print the bugid that introduced the regression

---
+++ mozregression/bugzilla.py
@@ -0,0 +1,19 @@
+import re
+from mozregression.json_pushes import JsonPushes
+
+RE_BUG_ID = re.compile('bug\s+(\d+)', re.I)
+
+
+def find_bugids_in_push(branch, changeset):
+    jp = JsonPushes(branch)
+    push = jp.pushlog_for_change(changeset, full='1')
+    branches = set()
+    for chset in push['changesets']:
+        res = RE_BUG_ID.search(chset['desc'])
+        if res:
+            branches.add(res.group(1))
+    return [b for b in branches]
+
+
+def bug_url(bugid):
+    return 'https://bugzilla.mozilla.org/show_bug.cgi?id=%s' % bugid

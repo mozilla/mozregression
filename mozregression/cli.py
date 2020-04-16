f270a3d636eb2871b46ab2cc9023cbba0de1260b
William Lachance <wlachance@mozilla.com>
2020-04-16 09:53:35 -0400

    Bug 1341976 - Clarify that mozregression is licensed under the MPL2 (#608)

--- mozregression/cli.py
+++ mozregression/cli.py
@@ -1,7 +1,3 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 """
 This module parses and checks the command line with :func:`cli` and return a
 :class:`Configuration` object that hold information for running the

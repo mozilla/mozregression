mozilla/mozregression

commit 6c7da9d1a97aea92591b7db3f407189444176c8b

Return full URLs from `url_links()` and use them when searching for builds
The way url_links was returning only the leaf node of the path rather than full
URLs was making it awkward to implement the firefox-l10n app, which needs to use
the function for two different URLs (one for the folder with the l10n builds,
and the other for the regular m-c nightly folder for the build info .txt file).

I also found it unpleasant on principle, especially given the result is then
immediately simply concatenated with the page URL to get an (hopefully) absolute
URL anyway, without proper URL parsing (pretty much guaranteed to work given the
known page structure to be fair, but still felt icky).  The new way is still not
perfect since there's no accounting for any hypothetical baseURI, but that seems
a distant concern.

mozregression/fetch_build_info.py
@@ -196,10 +196,10 @@ class NightlyInfoFetcher(InfoFetcher):
         if not url.endswith("/"):
             url += "/"
         for link in url_links(url):
-            if "build_url" not in data and self.build_regex.match(link):
-                data["build_url"] = url + link
-            elif "build_txt_url" not in data and self.build_info_regex.match(link):
-                data["build_txt_url"] = url + link
+            if "build_url" not in data and self.build_regex.search(link):
+                data["build_url"] = link
+            elif "build_txt_url" not in data and self.build_info_regex.search(link):
+                data["build_txt_url"] = link
         if data:
             # Check that we found all required data. The URL in build_url is
             # required. build_txt_url is optional.
@@ -233,8 +233,8 @@ class NightlyInfoFetcher(InfoFetcher):
         # first parse monthly list to get correct directory
         matches = []
         for dirlink in month_links:
-            if link_regex.match(dirlink):
-                matches.append(url + dirlink)
+            if link_regex.search(dirlink):
+                matches.append(dirlink)
         # the most recent build urls first
         matches.reverse()
         return matches

tests/unit/test_fetch_build_info.py
@@ -48,10 +48,10 @@ class TestNightlyInfoFetcher(unittest.TestCase):
     @patch("mozregression.fetch_build_info.url_links")
     def test__find_build_info_from_url(self, url_links):
         url_links.return_value = [
-            "file1.txt.gz",
-            "file2.txt",
-            "firefox01linux-x86_64.txt",
-            "firefox01linux-x86_64.tar.bz2",
+            "http://foo/file1.txt.gz",
+            "http://foo/file2.txt",
+            "http://foo/firefox01linux-x86_64.txt",
+            "http://foo/firefox01linux-x86_64.tar.bz2",
         ]
         expected = {
             "build_txt_url": "http://foo/firefox01linux-x86_64.txt",
@@ -83,10 +83,11 @@ class TestNightlyInfoFetcher(unittest.TestCase):
     @patch("mozregression.fetch_build_info.url_links")
     def test__get_url(self, url_links):
         url_links.return_value = [
-            "2014-11-01-03-02-05-mozilla-central/",
-            "2014-11-01-03-02-05-foo/",
-            "foo",
-            "bar/",
+            fetch_configs.ARCHIVE_BASE_URL
+            + "/firefox/nightly/2014/11/2014-11-01-03-02-05-mozilla-central/",
+            fetch_configs.ARCHIVE_BASE_URL + "/firefox/nightly/2014/11/2014-11-01-03-02-05-foo/",
+            fetch_configs.ARCHIVE_BASE_URL + "/firefox/nightly/2014/11/foo",
+            fetch_configs.ARCHIVE_BASE_URL + "/firefox/nightly/2014/11/bar/",
         ]
         urls = self.info_fetcher._get_urls(datetime.date(2014, 11, 1))
         self.assertEqual(

mozregression/fetch_configs.py
@@ -285,7 +285,7 @@ class NightlyConfigMixin(metaclass=ABCMeta):
 
     def _get_nightly_repo_regex(self, date, repo):
         if isinstance(date, datetime.datetime):
-            return r"^%04d-%02d-%02d-%02d-%02d-%02d-%s/$" % (
+            return r"/%04d-%02d-%02d-%02d-%02d-%02d-%s/$" % (
                 date.year,
                 date.month,
                 date.day,
@@ -294,7 +294,7 @@ class NightlyConfigMixin(metaclass=ABCMeta):
                 date.second,
                 repo,
             )
-        return r"^%04d-%02d-%02d-[\d-]+%s/$" % (date.year, date.month, date.day, repo)
+        return r"/%04d-%02d-%02d-[\d-]+%s/$" % (date.year, date.month, date.day, repo)
 
     def can_go_integration(self):
         """

mozregression/network.py
@@ -5,6 +5,7 @@ network functions utilities for mozregression.
 from __future__ import absolute_import
 
 import re
+from urllib.parse import urljoin
 
 import redo
 import requests
@@ -68,13 +69,6 @@ def get_http_session():
 def url_links(url, regex=None, auth=None):
     """
     Returns a list of links that can be found on a given web page.
-
-    Note that it href of links found are starting with '/' (absolute url from
-    the server point of view) then only the last part of the link will be
-    returned. E.G:
-
-    '/pub/firefox/nightly/2015/03/2015-03-02-03-02-04-mozilla-central/'
-    will return '2015-03-02-03-02-04-mozilla-central/'.
     """
     response = retry_get(url, auth=auth)
     response.raise_for_status()
@@ -94,12 +88,7 @@ def url_links(url, regex=None, auth=None):
     result = []
     for link in soup.findAll("a"):
         href = link.get("href")
-        # return "relative" part of the url only
-        if href.startswith("/"):
-            if href.endswith("/"):
-                href = href.strip("/").rsplit("/", 1)[-1] + "/"
-            else:
-                href = href.rsplit("/", 1)[-1]
-        if match(href):
-            result.append(href)
+        ahref = urljoin(url, href)
+        if match(ahref):
+            result.append(ahref)
     return result

tests/unit/test_fetch_configs.py
@@ -65,21 +65,21 @@ class TestFirefoxConfigLinux64(unittest.TestCase):
 
     def test_nightly_repo_regex(self):
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2008, 6, 15))
-        self.assertEqual(repo_regex, "^2008-06-15-[\\d-]+trunk/$")
+        self.assertEqual(repo_regex, "/2008-06-15-[\\d-]+trunk/$")
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2008, 6, 27))
-        self.assertEqual(repo_regex, "^2008-06-27-[\\d-]+mozilla-central/$")
+        self.assertEqual(repo_regex, "/2008-06-27-[\\d-]+mozilla-central/$")
         # test with a datetime instance (buildid)
         repo_regex = self.conf.get_nightly_repo_regex(datetime.datetime(2015, 11, 27, 6, 5, 58))
-        self.assertEqual(repo_regex, "^2015-11-27-06-05-58-mozilla-central/$")
+        self.assertEqual(repo_regex, "/2015-11-27-06-05-58-mozilla-central/$")
 
     def test_set_repo(self):
         self.conf.set_repo("foo-bar")
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2008, 6, 27))
-        self.assertEqual(repo_regex, "^2008-06-27-[\\d-]+foo-bar/$")
+        self.assertEqual(repo_regex, "/2008-06-27-[\\d-]+foo-bar/$")
         # with a value of None, default is applied
         self.conf.set_repo(None)
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2008, 6, 27))
-        self.assertEqual(repo_regex, "^2008-06-27-[\\d-]+mozilla-central/$")
+        self.assertEqual(repo_regex, "/2008-06-27-[\\d-]+mozilla-central/$")
 
 
 class TestFirefoxConfigLinux32(TestFirefoxConfigLinux64):
@@ -124,19 +124,19 @@ class TestThunderbirdConfig(unittest.TestCase):
 
     def test_nightly_repo_regex_before_2008_07_26(self):
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2008, 7, 25))
-        self.assertEqual(repo_regex, "^2008-07-25-[\\d-]+trunk/$")
+        self.assertEqual(repo_regex, "/2008-07-25-[\\d-]+trunk/$")
 
     def test_nightly_repo_regex_before_2009_01_09(self):
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2009, 1, 8))
-        self.assertEqual(repo_regex, "^2009-01-08-[\\d-]+comm-central/$")
+        self.assertEqual(repo_regex, "/2009-01-08-[\\d-]+comm-central/$")
 
     def test_nightly_repo_regex_before_2010_08_21(self):
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2010, 8, 20))
-        self.assertEqual(repo_regex, "^2010-08-20-[\\d-]+comm-central-trunk/$")
+        self.assertEqual(repo_regex, "/2010-08-20-[\\d-]+comm-central-trunk/$")
 
     def test_nightly_repo_regex_since_2010_08_21(self):
         repo_regex = self.conf.get_nightly_repo_regex(datetime.date(2010, 8, 21))
-        self.assertEqual(repo_regex, "^2010-08-21-[\\d-]+comm-central/$")
+        self.assertEqual(repo_regex, "/2010-08-21-[\\d-]+comm-central/$")
 
 
 class TestThunderbirdConfigWin(TestThunderbirdConfig):

tests/unit/test_network.py
@@ -47,7 +47,7 @@ class TestUrlLinks(unittest.TestCase):
         </body>
         """
         )
-        self.assertEqual(network.url_links(""), ["thing/", "thing2"])
+        self.assertEqual(network.url_links(""), ["/useless/thing/", "/useless/thing2"])
 
 
 def test_set_http_session():

commit 3d34f72e5903ab9fbd24a4f1a3f71b1ed6106a63

Bug 1657225 - Check nightly build regexs against filenames, not whole URLs
Commit 6c7da9d1a97aea92591b7db3f407189444176c8b introduced a regression with
Thunderbird where it was downloading jsshell ZIPs on Windows instead of
Thunderbird builds, because the app name in the regex was matching against a
folder name in the URL, and on TB nightly archive pages jsshell is listed first
so gets picked.

mozregression/fetch_build_info.py
@@ -202,9 +202,10 @@ class NightlyInfoFetcher(InfoFetcher):
         if not self.fetch_config.has_build_info:
             links += url_links(self.fetch_config.get_nightly_info_url(url))
         for link in links:
-            if "build_url" not in data and self.build_regex.search(link):
+            name = os.path.basename(link)
+            if "build_url" not in data and self.build_regex.match(name):
                 data["build_url"] = link
-            elif "build_txt_url" not in data and self.build_info_regex.search(link):
+            elif "build_txt_url" not in data and self.build_info_regex.match(name):
                 data["build_txt_url"] = link
         if data:
             # Check that we found all required data. The URL in build_url is

tests/unit/test_fetch_build_info.py
@@ -132,6 +132,31 @@ bar/nightly/2014/11/2014-11-15-01-02-05-mozilla-central/",
             self.info_fetcher.find_build_info(datetime.date(2014, 11, 15))
 
 
+class TestNightlyInfoFetcher2(unittest.TestCase):
+    def setUp(self):
+        fetch_config = fetch_configs.create_config("firefox", "win", 64, "x86_64")
+        self.info_fetcher = fetch_build_info.NightlyInfoFetcher(fetch_config)
+
+    @patch("mozregression.fetch_build_info.url_links")
+    def test__find_build_info_from_url(self, url_links):
+        url_links.return_value = [
+            "http://foo/firefox/jsshell-win64.zip",
+            "http://foo/file1.txt.zip",
+            "http://foo/file2.txt",
+            "http://foo/firefox01linux-x86_64.txt",
+            "http://foo/firefox01linux-x86_64.tar.bz2",
+            "http://foo/firefox01win64.txt",
+            "http://foo/firefox01win64.zip",
+        ]
+        expected = {
+            "build_txt_url": "http://foo/firefox01win64.txt",
+            "build_url": "http://foo/firefox01win64.zip",
+        }
+        builds = []
+        self.info_fetcher._fetch_build_info_from_url("http://foo", 0, builds)
+        self.assertEqual(builds, [(0, expected)])
+
+
 class TestIntegrationInfoFetcher(unittest.TestCase):
     def setUp(self):
         fetch_config = fetch_configs.create_config("firefox", "linux", 64, "x86_64")
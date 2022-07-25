dca7ed646abeb68d11f5cb4c3f2803b2ca193f67
Zeid Zabaneh <2043828+zzzeid@users.noreply.github.com>
2022-07-25 15:19:40 -0400

    tests: set qt qpa platform to offscreen for gui tests (bug 1780041) (#1072)

--- gui/tests/__init__.py
+++ gui/tests/__init__.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
 from PySide2.QtWidgets import QApplication
+
+if sys.platform == "linux":
+    os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 APP = QApplication([])  # we need an application to create widgets

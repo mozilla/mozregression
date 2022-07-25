98203cf26a4e38f779ab4a6e21df9e3fc996b7b1
Zeid Zabaneh <zeid@mozilla.com>
2022-07-25 15:00:12 -0400

    code review feedback

--- gui/tests/__init__.py
+++ gui/tests/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
-if sys.platform != "darwin":
+if sys.platform == "linux":
     os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 APP = QApplication([])  # we need an application to create widgets

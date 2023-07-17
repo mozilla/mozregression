db86f5d5256a8367a6e5a8dac9b16efca09be616
Zeid Zabaneh <zeid@mozilla.com>
2023-07-17 09:25:55 -0400

    setup: update programming language classifiers (bug 1843854)

--- setup.py
+++ setup.py
@@ -52,9 +52,10 @@ setup(
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
     ],
 )

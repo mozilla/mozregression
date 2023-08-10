82673d8670c57ba93f439858b5bd817989573d2c
Zeid Zabaneh <zeid@mozilla.com>
2023-08-10 17:12:14 -0400

    parse lsof output in a separate method, fix comparison

--- gui/splash_hook.py
+++ gui/splash_hook.py
@@ -6,7 +6,7 @@ import subprocess
 
 class Splash:
     # Milliseconds to pass before re-checking open files.
-    CHECK_MILLISECONDS = 3000
+    CHECK_MILLISECONDS = 500
 
     def __init__(self):
         # Must import here to avoid multiprocess forking issue with Core Foundation.
@@ -28,6 +28,20 @@ class Splash:
         self.win.after(self.CHECK_MILLISECONDS, self.check_doneness)
         self.win.mainloop()
 
+    def get_lsof(self):
+        """Return a list of files that are extracted by the bootloader."""
+        out = subprocess.check_output(["lsof", "-p", self.ppid])
+        lines = out.splitlines()
+
+        # _MEI is the prefix used in the temporary folder when extracting. See
+        # pyinstaller.org/en/stable/operating-mode.html#how-the-one-file-program-works.
+        relevant_lines = [line for line in lines if b"_MEI" in line]
+
+        # Return only the filenames.
+        parsed_lines = [line.split(b" ")[-1] for line in relevant_lines]
+        parsed_lines.sort()
+        return parsed_lines
+
     def check_doneness(self):
         """Check whether the bootloader has finished loading files."""
 
@@ -35,12 +49,11 @@ class Splash:
         # and if it has not changed between those two checks, assume extraction is
         # complete.
 
-        out = subprocess.check_output(["lsof", "-p", self.ppid])
-        if not self.last_lsof:
-            self.last_lsof = out
-        if out == self.last_lsof:
+        parsed_lsof = self.get_lsof()
+        if parsed_lsof == self.last_lsof:
             self.win.destroy()
         else:
+            self.last_lsof = parsed_lsof
             self.win.after(self.CHECK_MILLISECONDS, self.check_doneness)
 
 

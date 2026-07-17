98c604cd843a0d9ef61882fbb5a1bdbb8f865309
Magnolia Liu <magnolia.liu@mail.utoronto.ca>
2026-07-17 12:23:46 -0400

    remove startswith repetition

--- mozregression/fetch_configs.py
+++ mozregression/fetch_configs.py
@@ -240,11 +240,12 @@ class CommonConfig(object):
         """
         if repo:
             name = branches.get_name(repo)
-            if self.app_name == "thunderbird" and name.startswith("esr"):
+            is_esr = name.startswith("esr")
+            if self.app_name == "thunderbird" and is_esr:
                 self.repo = "comm-" + name
-            elif self.app_name == "firefox" and name.startswith("esr"):
+            elif self.app_name == "firefox" and is_esr:
                 self.repo = "mozilla-" + name
-            elif name.startswith("esr"):
+            elif is_esr:
                 # A bare `esr` defaults to the Firefox (`mozilla`) repository.
                 self.repo = "mozilla-" + name
             else:
